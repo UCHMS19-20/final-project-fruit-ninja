a71b96799f60cca56d3743edf275b4537c29d7bf
KevinCav8 <kcavicchia@ucvts.org>
2020-01-11 11:37:30 -0500

    Added multiple bombs.

--- src/finalproject.py
+++ src/finalproject.py
@@ -6,7 +6,7 @@ window = turtle.Screen()
 window.title("FRUIT NINJA")
 window.bgcolor("brown")
 window.setup(width=800, height=600)
-
+window.tracer(0)
 
 
 user = turtle.Turtle()
@@ -38,27 +38,36 @@ turtle.listen()
 turtle.onkey(move_left, "Left")
 turtle.onkey(move_right, "Right")
 
-bomb = turtle.Turtle()
-bomb.shape("circle")
-bomb.color("black")
-bomb.speed(0)
-bomb.penup()
-bomb.setposition(0, 300)
+bombs = []
+
+for _ in range(10):
+    bomb = turtle.Turtle()
+    bomb.shape("circle")
+    bomb.color("black")
+    bomb.speed(0)
+    bomb.penup()
+    bomb.setposition(0, 300)
+    bomb.speed = random.randint(3,5)
+    bombs.append(bomb)
 
 while True:
-    y = bomb.ycor()
-    y -= 3
-    bomb.sety(y)
+ 
+    window.update()
+
+    for bomb in bombs:
+        y = bomb.ycor()
+        y -= bomb.speed
+        bomb.sety(y)
+        
+        if y < -300:
+            x = random.randint(-380,380)
+            y = random.randint(300,400)
+            bomb.goto(x,y)
     
-    if y < -300:
-        x = random.randint(-380,380)
-        y = random.randint(300,400)
-        bomb.goto(x,y)
-    
-    if bomb.distance(user) < 20:
-        x = random.randint(-380,380)
-        y = random.randint(300,400)
-        bomb.goto(x,y)
+        if bomb.distance(user) < 20:
+            x = random.randint(-380,380)
+            y = random.randint(300,400)
+            bomb.goto(x,y)
 
 
 window.mainloop()
